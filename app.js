8270911590deabdbd9a9c699d0a2c8629987f107
easyAppsRepos <jralfarog@gmail.com>
2018-04-23 03:44:32 -0600

    test1ds

--- app.js
+++ app.js
@@ -64,6 +64,12 @@ expressApp.get('/categoriasHome', function(req, res) {
 });
 
 
+expressApp.get('/categoriasActivas', function(req, res) {
+    db(`SELECT  s.* FROM categoria as s WHERE s.estado = 1`).then((data) => {
+      console.log(data);
+      res.json(data);
+    }).catch(err => res.send(err).status(500));
+});
 
 
 
