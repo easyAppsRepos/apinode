203d6bb41d81c213b65a0f8c58765811318d83de
easyAppsRepos <jralfarog@gmail.com>
2018-05-14 23:47:40 -0600

    addservicioss

--- app.js
+++ app.js
@@ -205,7 +205,14 @@ WHERE x.idServicio = sc.idServicio AND sc.idCita = r.idCita
 
 
             var groups = _.groupBy(data, 'soloFecha');
-            return res.send(groups);
+
+            var sorted = _.sortBy(groups,function(node){
+  return - (new Date(node).getTime());
+});
+
+console.log(sorted);
+
+            return res.send(sorted);
 
 
       }).catch(err => res.send(err).status(500));
