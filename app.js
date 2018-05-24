4dbb1375c7785116b0753a389384d10097f9248c
easyAppsRepos <jralfarog@gmail.com>
2018-05-23 21:37:41 -0600

    buscar2

--- app.js
+++ app.js
@@ -665,6 +665,26 @@ WHERE  c.fechaExpira > CURRENT_TIMESTAMP AND c.estado = 1  ORDER BY c.porcentaje
 
 
 
+    expressApp.post('/', (req, res) => {
+     Promise.all([
+    db(`SELECT c.idCentro, c.nombre, c.direccion, c.idFoto, c.telefono,c.latitud, c.longitud, 
+      ci.idCita, ci.estado, ci.notaCita, ci.comentarioEstado, ci.idEmpleado, ci.horaInicio,
+      ci.horaFinalEsperado,precioEsperado, ci.idCuponCliente, ci.idCliente FROM centro as c, cita as ci 
+      WHERE ci.idCita = ? AND c.idCentro = ci.idCentro`,[req.body.idCita]),
+    db(`SELECT s.idServicio, s.nombre, s.duracion, s.precio, s.idCategoria, s.descripcion, c.nombre as nombreCategoria  
+      FROM servicio as s, categoria as c, servicio_cita as sc  
+      WHERE s.idServicio = sc.idServicio AND sc.idCita = ? AND c.idCategoria = s.idCategoria AND s.estado = 1`,[req.body.idCita])
+    ])
+      .then((data) => {
+
+        if (!data) res.send().status(500);
+    //var groups = _.groupBy(data[0], 'nombreCategoria');
+  
+        return res.send({cita:data[0], servicios:data[1]});
+      }).catch(err => res.send(err).status(500));
+  });
+
+
 
   expressApp.post('/getCentroServicios', (req, res) => {
      Promise.all([
