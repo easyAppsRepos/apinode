2c81c360fee0e051e80683c5e7d78f835432311b
easyAppsRepos <jralfarog@gmail.com>
2018-05-26 02:23:14 -0600

    addservicioss

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const storage = multer.diskStorage({
     cb(null, './uploads/');
   },
   filename: function(req, file, cb) {
-    cb(null,  file.fieldname + '-' + Date.now());
+    cb(null,  file.fieldname + '-' + Date.now()+ path.extname(file.originalname));
   }
 });
 
