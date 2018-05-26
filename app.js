bf437321b87e3341fd1549e972c25ede9ae56e53
easyAppsRepos <jralfarog@gmail.com>
2018-05-26 02:04:00 -0600

    addservicioss

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const path = require('path');
 const multer  =   require('multer');
 
 
-const upload = multer({dest: 'uploads/'});
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null,  file.filename);
+  }
+});
+
+
+
+const upload = multer({storage: storage});
 
 
 const cors = require('cors');
@@ -56,7 +68,7 @@ const app = () => {
 
 
   const expressApp = express();
-  expressApp.use('/uploads',express.static('uploads'));
+  expressApp.use('/uploads',express.static('uploads')); 
   expressApp.use(bodyParser.urlencoded({ extended: true }));
   expressApp.use(bodyParser.json());
 //expressApp.use(cors({origin: 'http://localhost:3003'}));
