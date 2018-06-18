19c5bd882e1989d5e7032df185286bafaa6a250d
easyAppsRepos <jralfarog@gmail.com>
2018-06-18 15:41:07 -0600

    oferta

--- app.js
+++ app.js
@@ -225,7 +225,7 @@ else{
       (SELECT ec.estado 
       FROM empleado_categoria as ec 
       WHERE ec.idCategoria = c.idCategoria 
-      AND ec.idEmpleado = ? AND ec.estado = 1) as estadoEmpleadoCategoria 
+      AND ec.idEmpleado = ? AND ec.estado = 1) as checked 
       FROM categoria as c WHERE c.estado = 1 
  `,[req.body.idEmpleado])
       .then((data) => {
