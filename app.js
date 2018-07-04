95995956a9c5bdf0facb5e7bd9e1087fa5195af3
easyAppsRepos <jralfarog@gmail.com>
2018-07-04 10:04:19 -0600

    push

--- app.js
+++ app.js
@@ -1222,6 +1222,25 @@ WHERE x.idServicio = sc.idServicio AND sc.idCita = r.idCita
       }).catch(err => res.send(err).status(500));
   });
 
+  expressApp.post('/buscarServiciosGPS2', (req, res) => {
+    db(`SELECT c.*, 
+      MAX(s.precio) as pMax, 
+      MIN(s.precio) as pMin, 
+      COUNT(DISTINCT ec.puntuacion) as cantRate, 
+      AVG(ec.puntuacion) as rate, 
+      ( 6371 * acos( cos( radians(?) ) * cos( radians( c.latitud ) ) 
+   * cos( radians(c.longitud) - radians(?)) + sin(radians(?)) 
+   * sin( radians(c.latitud)))) AS distance 
+      FROM servicio as s, centro as c LEFT JOIN evaluacionCentro as ec ON ec.idCentro = c.idCentro
+      WHERE c.idCentro = s.idCentro 
+      AND s.idSubcategoria = ? 
+      AND s.estado = 1 
+      GROUP BY c.idCentro`,[req.body.lat, req.body.lon, req.body.lat, req.body.idSubcategoria])
+      .then((data) => {
+        if (!data) res.send().status(500);
+        return res.send(data);
+      }).catch(err => res.send(err).status(500));
+  });
 
 
   expressApp.post('/getCentrosMapa', (req, res) => {
