fd2907e9622166312ccb03d36fa82d7aacd9d78f
easyAppsRepos <jralfarog@gmail.com>
2018-07-10 08:53:39 -0600

    e2

--- app.js
+++ app.js
@@ -641,6 +641,18 @@ WHERE x.idServicio = sc.idServicio AND sc.idCita = r.idCita
       }).catch(err => res.send(err).status(500));
   });
 
+
+        expressApp.post('/borrarRmanual', function(req, res) {
+     db(`DELETE FROM reservaManual WHERE idReservaManual = ?`,[req.body.idReservaManual])
+      .then((data) => {
+         if (!data) res.send().status(500);
+        return res.send(data);
+
+      }).catch(err => res.send(err).status(500));
+  });
+
+
+
         expressApp.post('/eliminarServicio', function(req, res) {
      db(`DELETE FROM servicio WHERE idServicio = ?`,[req.body.idServicio])
       .then((data) => {
