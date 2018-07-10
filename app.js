5ed45f54462ada679d5f551bca148563e1ef5130
easyAppsRepos <jralfarog@gmail.com>
2018-07-10 00:54:53 -0600

    e2

--- app.js
+++ app.js
@@ -245,7 +245,17 @@ else{
       }).catch(err => res.send(err).status(500));
   });
 
+ expressApp.post('/citasUserFiltroSA', (req, res) => {
+    db("SELECT  df.nombre as nombreCentro, df.idFoto, r.precioEsperado, em.nombre as nombreEmpleado, r.idCita, r.idCentro, CONCAT(DATE_FORMAT(r.`horaInicio`, '%d/%m/%y %H:%i'), ' - ', DATE_FORMAT(r.`horaFinalEsperado`, '%H:%i')) as FechaCita, r.comentarioCita,r.comentarioEstado, r.notaCita, r.estado, (SELECT cupon.porcentajeDescuento FROM cupon, cupon_cliente as gh WHERE gh.idCupon = cupon.idCupon AND gh.idCuponCliente = r.idCuponCliente) as descuento, (SELECT COUNT(sc.idServicioCita) FROM servicio_cita as sc WHERE sc.idCita = r.idCita AND sc.estado = 0) as totalServicios, (SELECT v.puntuacion FROM evaluacionCentro as v WHERE v.idCita = r.idCita LIMIT 1) as valoracion  FROM centro as df, cita as r LEFT JOIN empleado as em ON r.idEmpleado = em.idEmpleado WHERE df.idCentro = r.idCentro AND r.idCliente  = ? AND DATE(r.horaInicio) BETWEEN ? AND ? ",[req.body.idCliente,req.body.fecha, req.body.fechaF])
+      .then((data) => {
+        if (!data) res.send().status(500);
 
+            var groups = _.groupBy(data, 'estado');
+            return res.send(groups);
+           //return res.send(data);
+
+      }).catch(err => res.send(err).status(500));
+  });
 
   expressApp.post('/citasCentroC', (req, res) => {
     db(`SELECT c.nombre as nombreCliente, r.precioEsperado, c.telefono, em.nombre as nombreEmpleado, (SELECT SUM(s.precio) FROM servicio as s, servicio_cita as sc WHERE sc.idServicio = s.idServicio AND sc.idCita = r.idCita) as total,
