7520c5c44e5e863bf7be87017540b6ae8accf102
easyAppsRepos <jralfarog@gmail.com>
2018-07-15 00:27:23 -0600

    rp

--- app.js
+++ app.js
@@ -25,44 +25,6 @@ const storage = multer.diskStorage({
 
 
 
-          nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass // generated ethereal password
-        }
-    });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"yourBeauty" <foo@example.com>', // sender address
-        to:'easyappsinfo@gmail.com', // list of receivers
-        subject: 'Recuperacion de contraseña ✔', // Subject line
-        text: 'Hemos recuperado tu contraseña! Tu contraseña yourBeaty nueva es:'
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-
-            if(error){
-            console.log('Error occured');
-            console.log(error.message);
-            //return;
-      
-            }
-
-          console.log(info);
-
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
-});
-
 /*
 const transport = nodemailer.createTransport("SMTP", {
         service: 'Gmail',
