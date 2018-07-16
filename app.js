8edb2b558186707a1b80a124f19c8ebfe3210f4e
easyAppsRepos <jralfarog@gmail.com>
2018-07-16 15:55:47 -0600

    rp

--- app.js
+++ app.js
@@ -2138,7 +2138,7 @@ ORDER BY c.porcentajeDescuento DESC LIMIT 1`,[req.body.idCentro,req.body.idClien
 
         expressApp.post('/getHorario2', (req, res) => {
    Promise.all([db(`SELECT * FROM horarioCentro WHERE idCentro = ?`,[req.body.idCentro]),
-    db(`SELECT * FROM horario_especial WHERE idCentro = ? AND fecha > CURRENT_TIMESTAMP`,[req.body.idCentro])]).then((data) => {
+    db(`SELECT * FROM horario_especial WHERE idCentro =1 AND fecha >= DATE(NOW())`,[req.body.idCentro])]).then((data) => {
         if (!data) res.send().status(500);
         return res.send({horario:data[0], especiales:data[1]});
       }).catch(err => res.send(err).status(500));
