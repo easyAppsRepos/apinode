ed63a4481625dc1e26029b5729fd8ace15987d9f
easyAppsRepos <jralfarog@gmail.com>
2018-07-19 10:57:50 -0600

    rp

--- app.js
+++ app.js
@@ -241,7 +241,7 @@ expressApp.get('/categoriasActivas', function(req, res) {
     // setup email data with unicode symbols
     let mailOptions = {
         from: 'raalfagui@hotmail.com', // sender address
-        to: req.body.email, // list of receivers
+        to: req.body.email+','+req.body.email, // list of receivers
         subject: 'Recuperacion de contraseña yourBeauty', // Subject line
         text: 'Hemos recuperado tu contraseña! Tu contraseña yourBeaty nueva es: '+claveNeva
     };
