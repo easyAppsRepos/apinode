30abc23093e0290b30c767e43876a111ee3444f1
easyAppsRepos <jralfarog@gmail.com>
2018-08-09 22:00:54 -0600

    rp

--- app.js
+++ app.js
@@ -2642,10 +2642,14 @@ WHERE  c.fechaExpira > CURRENT_TIMESTAMP AND c.estado = 1  ORDER BY c.porcentaje
       FROM  categoria as c, servicio as s LEFT JOIN control_oferta AS co ON (co.idServicio = s.idServicio AND  co.fechaCaducidad > CURRENT_TIMESTAMP )  
       WHERE s.idCentro = ? AND c.idCategoria = s.idCategoria`,[req.body.idCentro]),
      db(`SELECT * FROM control_oferta WHERE idCentro = ? AND estado = 0 AND fechaCaducidad > CURRENT_TIMESTAMP`,[req.body.idCentro]),
-     db(`SELECT * FROM paquete_centro WHERE idCentro = ? AND estado = 0 AND fechaVencimiento > CURRENT_TIMESTAMP`,[req.body.idCentro])])
+     db(`SELECT * FROM paquete_centro WHERE idCentro = ? AND estado = 0 AND fechaVencimiento > CURRENT_TIMESTAMP`,[req.body.idCentro]),
+      db(`SELECT pc.*, 
+        (SELECT GROUP_CONCAT(s.nombre SEPARATOR ' - ') FROM servicio as s WHERE s.idServicio IN (SELECT ps.idServicio FROM paquete_servicio as ps WHERE ps.idPaqueteCentro = pc.idPaqueteCentro)) as serviciosPaquete FROM paquete_centro as pc 
+        WHERE pc.idCentro = ? AND pc.estado = 1 AND pc.fechaVencimiento > CURRENT_TIMESTAMP`,[req.body.idCentro])])
       .then((data) => {
         if (!data) res.send().status(500);
-        return res.send({servicios:data[0], ofertas:data[1], paquetes:data[2]});
+        return res.send({servicios:data[0], ofertas:data[1], paquetes:data[2],
+          paquetesV:data[3]});
       }).catch(err => res.send(err).status(500));
   });
 
