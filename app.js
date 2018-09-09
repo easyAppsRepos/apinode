e6b0ebc37426a338fba9bbc0689d33bfa2c614f5
easyAppsRepos <jralfarog@gmail.com>
2018-09-09 11:11:49 -0600

    rps

--- app.js
+++ app.js
@@ -2951,8 +2951,8 @@ ORDER BY c.porcentajeDescuento DESC LIMIT 1`,[req.body.idCentro,req.body.idClien
 
         expressApp.post('/serviciosC', function(req, res) {
 
-    db(`SELECT s.idServicio, s.nombre, s.duracion, s.precio, s.idCategoria, s.descripcion, c.nombre as nombreCategoria, cs.nombre as nombreSubcategoria, √,  (SELECT precioOferta FROM control_oferta as co WHERE co.idServicio = s.idServicio AND co.fechaCaducidad>CURRENT_TIMESTAMP  LIMIT 1) as oferta    
-      FROM servicio as s, categoria as c, subcategoria as cs    
+    db(`SELECT s.idServicio, s.nombre, s.duracion, s.precio, s.idCategoria, s.idSubcategoria, s.descripcion, c.nombre as nombreCategoria, cs.nombre as nombreSubcategoria, ,  (SELECT precioOferta FROM control_oferta as co WHERE co.idServicio = s.idServicio AND co.fechaCaducidad>CURRENT_TIMESTAMP  LIMIT 1) as oferta    
+      FROM servicio as s, categoria as c, subcategoria as cs  
       WHERE s.idCentro = ? AND c.idCategoria = s.idCategoria AND cs.idSubcategoria = s.idSubcategoria AND s.estado = 1`,[req.body.idCentro]).then((data) => {
          if (!data) res.send().status(500);
         return res.send(data);
