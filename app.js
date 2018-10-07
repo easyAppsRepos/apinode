3b870b2e9599e95fc43fc4a854727af4faf7e7c3
easyAppsRepos <jralfarog@gmail.com>
2018-10-07 11:32:35 -0600

    rps

--- app.js
+++ app.js
@@ -2792,12 +2792,12 @@ WHERE  c.fechaExpira > CURRENT_TIMESTAMP AND c.estado = 1  ORDER BY c.porcentaje
     //let horaFinal = req.body.fecha;
    // let horaFinal = moment(req.body.fecha).format("YYYY-MM-DD");
     let idCita=null;
-
+    let cliR = req.body.clienteReferencia || ' ';
     db(`INSERT INTO cita (idCentro, idCliente, horaInicio, horaFinalEsperado, precioEsperado,
-      notaCita, estado,idCuponCliente ) 
-        VALUES (?,?,?,?,?,?,?,?)
+      notaCita, estado,idCuponCliente, clienteReferencia ) 
+        VALUES (?,?,?,?,?,?,?,?,?)
         `,[req.body.idCentro, req.body.idCliente,req.body.fechaInicio,
-        req.body.fechaFinal,req.body.total, (req.body.notaCita || ' '), 1, req.body.idCuponCliente])
+        req.body.fechaFinal,req.body.total, (req.body.notaCita || ' '), 1, req.body.idCuponCliente, cliR])
       .then((data) => {
         console.log(data);
         if (!data) {
