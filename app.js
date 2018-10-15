831475065d74e6f5225cf3679420d116df302fa5
easyAppsRepos <jralfarog@gmail.com>
2018-10-15 04:51:38 -0600

    rpssds

--- app.js
+++ app.js
@@ -4207,10 +4207,12 @@ WHERE he.diaSemana = hc.diaSemana AND he.idEmpleado IN (SELECT idEmpleado FROM e
 
 
     expressApp.post('/getServiciosCita', (req, res) => {
-     Promise.all([db(`SELECT vv.precioEsperado, sc.idServicioCita, sc.precioCobrado, s.idServicio, s.nombre, s.duracion, s.precio, s.idCategoria, s.descripcion, c.nombre as nombreCategoria  
-      FROM cita as vv, servicio as s, categoria as c, servicio_cita as sc  
-      WHERE s.idServicio = sc.idServicio 
-      AND sc.idCita = ? AND c.idCategoria = s.idCategoria AND vv.idCita = ? AND s.estado = 1`,[req.body.idCita,req.body.idCita]),
+     Promise.all([db(`SELECT vv.precioEsperado, sc.idServicioCita, sc.estado, em.nombre as nombreEmpleado, sc.precioCobrado, CONCAT(DATE_FORMAT(sc.horaInicio, '%l:%i  %p'), ' - ', 
+       DATE_FORMAT(sc.horaFin, '%l:%i  %p')) as horaCita,
+        s.idServicio, s.nombre, s.duracion, s.precio, s.idCategoria, s.descripcion, c.nombre as nombreCategoria  
+      FROM cita as vv, servicio as s, categoria as c, servicio_cita as sc, empleado as em   
+      WHERE s.idServicio = sc.idServicio AND em.idEmpleado = sc.idEmpleado 
+      AND sc.idCita = 116 AND c.idCategoria = s.idCategoria AND vv.idCita = 116 AND s.estado = 1`,[req.body.idCita,req.body.idCita]),
      db(`SELECT s.idServicio, s.nombre, s.duracion, s.precio, s.idCategoria, s.descripcion, s.estado, c.nombre as nombreCategoria  
       FROM servicio as s, categoria as c 
       WHERE s.idCentro = ? AND c.idCategoria = s.idCategoria AND s.estado =  1`,[req.body.idCentro])])
