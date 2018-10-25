343f73f9a54790461c7a362fb765d963207c5667
easyAppsRepos <jralfarog@gmail.com>
2018-10-24 18:46:03 -0600

    rpssdssss

--- app.js
+++ app.js
@@ -4310,8 +4310,9 @@ idCentro,fecha,abierto,estado,fechaCreacion,timespan FROM horario_especial WHERE
       WHERE se.idServicio = s.idServicio AND se.idEmpleado = ? AND se.estado = 1) as idServicioEmpleado 
       FROM servicio as s, categoria as c WHERE s.idCentro = ? AND c.idCategoria = s.idCategoria`,[req.body.idEmpleado, req.body.idCentro]),
       db(`SELECT * FROM horarioEmpleado WHERE idEmpleado = ?`,[req.body.idEmpleado]),
-      db(`SELECT idEmpleadoBloqueLibre, idEmpleado, DATE_FORMAT(fechaInicio,'%Y-%m-%d') as fechaInicio, 
-        DATE_FORMAT(fechaFinal,'%Y-%m-%d') as fechaFinal FROM empleadoBloqueLibre WHERE idEmpleado = ?`,[req.body.idEmpleado])])
+      db(`SELECT *, TIME_FORMAT(horaEntrar, '%h:%i%p') as horaFI, 
+TIME_FORMAT(horaSalir, '%h:%i%p') as horaFF FROM horario_especial_empleado WHERE idEmpleado = ? AND 
+        CURDATE() <= fecha`,[req.body.idEmpleado])])
       .then((data) => {
          if (!data) res.send().status(500);
 
