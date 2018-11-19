95e0c23bd45f4c1f2d2182d840d884c6bfdf71a7
easyAppsRepos <jralfarog@gmail.com>
2018-11-18 19:45:42 -0600

    rpssdsssdfsss

--- app.js
+++ app.js
@@ -2969,8 +2969,9 @@ LEFT JOIN servicio_cita as c ON (c.idEmpleado = e.idEmpleado AND c.estado IN (0,
 
 
     expressApp.post('/cancelarRMAS', function(req, res) {
-     db(`DELETE FROM cita as c, servicio_cita as sc 
-      WHERE c.idCita = ? AND sc.idCita = ?`,[req.body.idCita,req.body.idCita])
+     db(`DELETE FROM cita as c 
+      INNER JOIN servicio_cita as sc 
+      ON c.idCita = sc.idCita  WHERE c.idCita =`,[req.body.idCita])
       .then((data) => {
          if (!data) res.send().status(500);
         return res.send(data);
