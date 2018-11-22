99bc35dad48ec2be05b9c1ba5b9e5023b9e8f761
easyAppsRepos <jralfarog@gmail.com>
2018-11-22 06:53:56 -0600

    rpssdsssdfssass

--- app.js
+++ app.js
@@ -2321,7 +2321,7 @@ funcionesBase.push(db(`SELECT ? as inicio, ? as fin, COUNT(DISTINCT e.idEmpleado
         WHERE e.idEmpleado IN (SELECT ec.idEmpleado FROM servicioEmpleado as ec 
         WHERE ec.idServicio = ? AND ec.estado = 1 ) AND e.idCentro = ? 
         AND (he.idEmpleado = e.idEmpleado AND he.diaSemana = ?
-         AND he.estado = 1 AND he.horaEntrar < ? AND he.horaSalir > ?)
+         AND he.estado = 1 AND he.horaEntrar < ? AND he.horaSalir > ? AND he.horaEntrar> CONVERT_TZ(now(),'+00:00','-05:00'))
         AND c.idServicioCita IS NULL
         AND rm.idReservaManual IS NULL HAVING disponibles > 0`,[inicioCita.format("YYYY-MM-DD HH:mm:ss"), 
         finCita.format("YYYY-MM-DD HH:mm:ss"),inicioCita.format("YYYY-MM-DD HH:mm:ss"), 
