a787f1dd106a019874cddc432046c0e47763d847
easyAppsRepos <jralfarog@gmail.com>
2018-12-17 04:10:59 -0600

    finalsdassxdss

--- app.js
+++ app.js
@@ -5842,7 +5842,7 @@ AND c.estado = 1`,[req.body.idCliente,moment(Date.now()).format("YYYY-MM-DD"), r
       FROM  centro as c LEFT JOIN evaluacionCentro as ec ON ec.idCentro = c.idCentro WHERE c.idCentro = ?
       GROUP BY c.idCentro`,[req.body.idCentro,ssdd,req.body.idCentro, req.body.idCliente, req.body.idCentro]), 
     db(`SELECT s.idServicio, s.nombre, s.duracion, s.idSubcategoria, sc.nombre as nombreSubcategoria, 
-      s.precio, s.idCategoria, c.idFoto as imagenCategoria, c.nombre as nombreCategoria, 
+      FORMAT(s.precio,2) as precio, s.idCategoria, c.idFoto as imagenCategoria, c.nombre as nombreCategoria, 
       ROUND((SELECT co.precioOferta FROM control_oferta AS co 
       WHERE co.idServicio = s.idServicio AND co.idCentro = ? 
       AND co.estado = 1 AND co.fechaCaducidad > CURRENT_TIMESTAMP LIMIT 1),2) as oferta  
